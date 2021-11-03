16f6fc1b7fcddf268b94303ae8346958c79b32df
yutatanomoki <yuta3071@gmail.com>
2021-11-03 21:23:04 +0900

    [fixed]recuest

--- slack_bot.rb
+++ slack_bot.rb
@@ -1,7 +1,10 @@
 require 'http'
 require 'json'
 
-response = HTTP.post("https://slack.com/api/auth.test", params: {
-  token: ENV['SLACK_API_TOKEN']
-})
+response = HTTP.post("https://slack.com/api/chat.postMessage", params: {
+    token: ENV['SLACK_API_TOKEN'],
+    channel: "#general",
+    text: "こんにちは！",
+    as_user: true,
+  })
 puts JSON.pretty_generate(JSON.parse(response.body))
