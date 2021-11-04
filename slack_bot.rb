7238a2ab2a74b761146bed7611066da1188552ac
yutatanomoki <yuta3071@gmail.com>
2021-11-04 18:57:53 +0900

    [add]並列処理

--- slack_bot.rb
+++ slack_bot.rb
@@ -1,5 +1,7 @@
 require 'http'
 require 'json'
+require 'eventmachine'
+require 'faye/websocket'
 
 response = HTTP.post("https://slack.com/api/rtm.start", params: {
   token: ENV['SLACK_API_TOKEN']
@@ -7,4 +9,27 @@ response = HTTP.post("https://slack.com/api/rtm.start", params: {
 
 rc = JSON.parse(response.body)
 
-puts rc['url']
+url = rc['url']
+
+EM.run do
+  # Web Socketインスタンスの立ち上げ
+  ws = Faye::WebSocket::Client.new(url)
+
+  #  接続が確立した時の処理
+  ws.on :open do
+    p [:open]
+  end
+
+  # RTM APIから情報を受け取った時の処理
+  ws.on :message do |event|
+    p [:message, JSON.parse(event.data)]
+  end
+
+  # 接続が切断した時の処理
+  ws.on :close do
+    p [:close, event.code]
+    ws = nil
+    EM.stop
+  end
+
+end
