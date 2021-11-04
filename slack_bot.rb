662230b14de00ac0fb8067b403f35205c1faccc7
yutatanomoki <yuta3071@gmail.com>
2021-11-04 18:09:38 +0900

    [fixed]url表示

--- slack_bot.rb
+++ slack_bot.rb
@@ -1,10 +1,10 @@
 require 'http'
 require 'json'
 
-response = HTTP.post("https://slack.com/api/chat.postMessage", params: {
-    token: ENV['SLACK_API_TOKEN'],
-    channel: "#general",
-    text: "こんにちは！",
-    as_user: true,
-  })
-puts JSON.pretty_generate(JSON.parse(response.body))
+response = HTTP.post("https://slack.com/api/rtm.start", params: {
+  token: ENV['SLACK_API_TOKEN']
+})
+
+rc = JSON.parse(response.body)
+
+puts rc['url']
